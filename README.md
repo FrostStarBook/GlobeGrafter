e437c0d53bdd28843fdfa9c35b1f324556c105ea
strawberry <liushuheng_ssss@163.com>
2023-08-07 18:18:27 +0800

    add proof contract and deploy to op test net

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # GlobeGrafter
 The map component on the full -chain is not just a map
+
+
+
+
+
+
+
+
+
+
+rogueLike-game contract(goerli-optimism):
+```shell
+GameAttrProof:
+0x627a72bbE16416Ae722BA05876C5cB2dcb0Dc6BB
+
+GameData:
+0x1aE9623899dDc2bB42217eF985a3d98E6E7623C1
+```
--- rogueLike-game/contract/GameData.sol
+++ rogueLike-game/contract/GameData.sol
@@ -1,34 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./GameAttrProof.sol";
+
 contract GameData {
     mapping(address => data) private players;
+
+    Groth16Verifier private gameAttrProof;
+
+    constructor() {
+        gameAttrProof = Groth16Verifier(0x627a72bbE16416Ae722BA05876C5cB2dcb0Dc6BB);
+    }
 
     struct data {
         uint _dungeons_level;
         uint _level;
         uint _exp;
-        uint _x;
-        uint _y;
         uint _damage;
         uint _attack;
         uint _gold;
-        uint _ac;
-        string _weapon;
     }
 
-    function save(uint dungeons_level, uint level, uint exp, uint x, uint y, uint damage, uint attack, uint gold, uint ac, string memory weapon) public {
+    function verifyAndSave(
+        uint[2] memory _pA,
+        uint[2][2] memory _pB,
+        uint[2] memory _pC,
+        uint[1] memory _pubSignals,
+        uint dungeons_level, uint level, uint exp, uint damage, uint attack, uint gold
+    ) public {
+        require(
+            gameAttrProof.verifyProof(_pA, _pB, _pC, _pubSignals),
+            "Invalid proof"
+        );
+        save(dungeons_level, level, exp, damage, attack, gold);
+    }
+
+    function save(uint dungeons_level, uint level, uint exp, uint damage, uint attack, uint gold) private {
         data storage playerData = players[msg.sender];
         playerData._dungeons_level = dungeons_level;
         playerData._level = level;
         playerData._exp = exp;
-        playerData._x = x;
-        playerData._y = y;
         playerData._damage = damage;
         playerData._attack = attack;
         playerData._gold = gold;
-        playerData._ac = ac;
-        playerData._weapon = weapon;
     }
 
     function getData() public view returns (data memory) {
