e3636edb9b4e8112c6febbb9e62fdb288c247cd6
strawberry <liushuheng_ssss@163.com>
2023-08-08 19:59:24 +0800

    update contract add UUPS and ERC721

--- rogueLike-game/contract/GameData.sol
+++ rogueLike-game/contract/GameData.sol
@@ -2,17 +2,35 @@
 pragma solidity ^0.8.0;
 
 import "./GameAttrProof.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 
-contract GameData {
-    mapping(address => data) private players;
+contract GameData is
+Initializable,
+UUPSUpgradeable,
+ERC721Upgradeable,
+ERC721URIStorageUpgradeable,
+OwnableUpgradeable{
+
+    uint256 public totalSupply;
+
+    mapping(address => Attributes) private players;
 
     Groth16Verifier private gameAttrProof;
 
-    constructor() {
+    function initialize() public initializer {
+        __ERC721_init("Hero NFT", "H");
+        __ERC721URIStorage_init();
+        __UUPSUpgradeable_init();
+        __Ownable_init();
         gameAttrProof = Groth16Verifier(0x627a72bbE16416Ae722BA05876C5cB2dcb0Dc6BB);
     }
 
-    struct data {
+    struct Attributes {
         uint _dungeons_level;
         uint _level;
         uint _exp;
@@ -32,11 +50,13 @@ contract GameData {
             gameAttrProof.verifyProof(_pA, _pB, _pC, _pubSignals),
             "Invalid proof"
         );
-        save(dungeons_level, level, exp, damage, attack, gold);
+        saveAttributes(dungeons_level, level, exp, damage, attack, gold);
+        mint(dungeons_level, level, exp, damage, attack, gold);
+
     }
 
-    function save(uint dungeons_level, uint level, uint exp, uint damage, uint attack, uint gold) private {
-        data storage playerData = players[msg.sender];
+    function saveAttributes(uint dungeons_level, uint level, uint exp, uint damage, uint attack, uint gold) private{
+        Attributes storage playerData = players[msg.sender];
         playerData._dungeons_level = dungeons_level;
         playerData._level = level;
         playerData._exp = exp;
@@ -45,7 +65,96 @@ contract GameData {
         playerData._gold = gold;
     }
 
-    function getData() public view returns (data memory) {
+    function getAttributes() public view returns (Attributes memory) {
         return players[msg.sender];
     }
+
+    event NFTMinted(
+        uint256 tokenId,
+        uint _dungeons_level,
+        uint _level,
+        uint _exp,
+        uint _damage,
+        uint _attack,
+        uint _gold
+    );
+
+    function mint(uint dungeons_level, uint level, uint exp, uint damage, uint attack, uint gold) private {
+        uint256 tokenId = totalSupply + 1;
+        string memory images;
+
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, tokenURI(tokenId, images));
+
+        totalSupply++;
+
+        emit NFTMinted(
+            tokenId,
+            dungeons_level,
+            level,
+            exp,
+            damage,
+            attack,
+            gold
+        );
+    }
+
+    function tokenURI(
+        uint256 tokenId,
+        string memory images
+    ) internal view returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        Attributes memory attributes = players[msg.sender];
+
+        string memory json = string(
+            abi.encodePacked(
+                "{",
+                '"name": "Hero NFT #',
+                Strings.toString(tokenId),
+                '",',
+                '"attributes": [',
+                '{ "trait_type": "_dungeons_level", "value": ',
+                Strings.toString(attributes._dungeons_level),
+                " },",
+                '{ "trait_type": "_level", "value": ',
+                Strings.toString(attributes._level),
+                " },",
+                '{ "trait_type": "_exp", "value": ',
+                Strings.toString(attributes._exp),
+                " },",
+                '{ "trait_type": "_damage", "value": ',
+                Strings.toString(attributes._damage),
+                " },",
+                '{ "trait_type": "_attack", "value": ',
+                Strings.toString(attributes._attack),
+                " },",
+                '{ "trait_type": "_gold", "value": ',
+                Strings.toString(attributes._gold),
+                " }",
+                "],",
+                '"image": "',
+                images,
+                '",',
+                '"background_color": "FFFFFF"',
+                "}"
+            )
+        );
+
+        return string(abi.encodePacked("data:application/json;base64,", Base64.encode(bytes(json))));
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    function _burn(uint256 id) internal override(ERC721Upgradeable, ERC721URIStorageUpgradeable) {
+        super._burn(id);
+    }
+
+    function tokenURI(
+        uint256 id
+    ) public view override(ERC721Upgradeable, ERC721URIStorageUpgradeable) returns (string memory) {
+        return super.tokenURI(id);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721Upgradeable, ERC721URIStorageUpgradeable) returns (bool) {}
 }
