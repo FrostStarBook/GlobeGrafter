e868c8b67513cf1822c015d34a8ef30815b2754b
strawberry <liushuheng_ssss@163.com>
2023-08-09 19:17:02 +0800

    update abi & add theGraph

---
+++ theGraph/game-data-statistics/src/contract.ts
@@ -0,0 +1,188 @@
+import {
+  AdminChanged as AdminChangedEvent,
+  Approval as ApprovalEvent,
+  ApprovalForAll as ApprovalForAllEvent,
+  BatchMetadataUpdate as BatchMetadataUpdateEvent,
+  BeaconUpgraded as BeaconUpgradedEvent,
+  Initialized as InitializedEvent,
+  MetadataUpdate as MetadataUpdateEvent,
+  NFTMinted as NFTMintedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  Transfer as TransferEvent,
+  Upgraded as UpgradedEvent
+} from "../generated/Contract/Contract"
+import {
+  AdminChanged,
+  Approval,
+  ApprovalForAll,
+  BatchMetadataUpdate,
+  BeaconUpgraded,
+  Initialized,
+  MetadataUpdate,
+  NFTMinted,
+  OwnershipTransferred,
+  Transfer,
+  Upgraded
+} from "../generated/schema"
+
+export function handleAdminChanged(event: AdminChangedEvent): void {
+  let entity = new AdminChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousAdmin = event.params.previousAdmin
+  entity.newAdmin = event.params.newAdmin
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.approved = event.params.approved
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+  let entity = new ApprovalForAll(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.operator = event.params.operator
+  entity.approved = event.params.approved
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleBatchMetadataUpdate(
+  event: BatchMetadataUpdateEvent
+): void {
+  let entity = new BatchMetadataUpdate(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity._fromTokenId = event.params._fromTokenId
+  entity._toTokenId = event.params._toTokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleBeaconUpgraded(event: BeaconUpgradedEvent): void {
+  let entity = new BeaconUpgraded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.beacon = event.params.beacon
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleInitialized(event: InitializedEvent): void {
+  let entity = new Initialized(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.version = event.params.version
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMetadataUpdate(event: MetadataUpdateEvent): void {
+  let entity = new MetadataUpdate(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity._tokenId = event.params._tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleNFTMinted(event: NFTMintedEvent): void {
+  let entity = new NFTMinted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.tokenId = event.params.tokenId
+  entity._dungeons_level = event.params._dungeons_level
+  entity._level = event.params._level
+  entity._exp = event.params._exp
+  entity._damage = event.params._damage
+  entity._attack = event.params._attack
+  entity._gold = event.params._gold
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleUpgraded(event: UpgradedEvent): void {
+  let entity = new Upgraded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.implementation = event.params.implementation
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
